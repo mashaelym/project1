a07c53da63ff9e5a33d041e7ed97966f64162bf5
mashaelym <ma735@njit.edu>
2017-10-13 12:44:25 -0400

    adding the string function class

---
+++ stringFunctions.php
@@ -0,0 +1,13 @@
+<?php
+class stringFunctions
+{
+	static public function printThis($inputText)
+        {
+               return print($inputText);
+         }
+	 static public function stringLength($text) 
+	 {
+		return strLen($text);
+	  }	
+    }
+?>
