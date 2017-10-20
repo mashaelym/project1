c66197d27ffcc5e1a5e5b667cd066e0c5e084170
mashaelym <ma735@njit.edu>
2017-10-20 12:51:43 -0400

    uploading latest version

--- HTMLTable.php
+++ HTMLTable.php
@@ -3,24 +3,22 @@
 
 class HTMLTable extends Page
 {
-  
+   
    public function get()
    {
      $request     = new Request();
-     $FileName = $this->getRequest()->getHTTPRequest()->getParameter('filename');
-     $parse       = new CSVParser('./uploads/' . $FileName);
-     $table       = '<table>';
-     $table      .= '<tr><td> test </td></tr>';
-     $table      .= '</table> ';
-     $this->html .= $table;
-     $this->html .=$parse;
+     $FileName    = $this->getRequest()->getHTTPRequest()->getParameter('filename');
+     $uploadPath = $this->getRequest()->getFileUploadRequest()->getUploadPath();
+     $parse       = CSVParser::parseCSV($uploadPath  . $FileName);
+     $tableHtmlOutput = ArrayToTable::generate($parse);
+   
+     $this->html .=$tableHtmlOutput;
    }
    
-   public function post()
+  public function post()
    {
-	   $this->html .= '<h1>Nothing to see here folks!</h1>';
+     $this->html .= '<h1>Nothing to see here folks!</h1>';
    }
-
 }
 
 ?>
